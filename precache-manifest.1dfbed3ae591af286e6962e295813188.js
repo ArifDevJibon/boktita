fa6ca9f371a3b838de1dacab639b4721fb445414
Arif Hossain <arif.dev.jibon@gamil.com>
2019-01-28 03:29:31 +0600

    Updates

---
+++ precache-manifest.1dfbed3ae591af286e6962e295813188.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e20474c07d6d9c787c96",
+    "url": "/ArifDevJibon/boktita/static/js/runtime~main.e20474c0.js"
+  },
+  {
+    "revision": "93cbbc11aa30b660d5e9",
+    "url": "/ArifDevJibon/boktita/static/js/main.93cbbc11.chunk.js"
+  },
+  {
+    "revision": "ac9ae1edcdde807d9986",
+    "url": "/ArifDevJibon/boktita/static/js/1.ac9ae1ed.chunk.js"
+  },
+  {
+    "revision": "93cbbc11aa30b660d5e9",
+    "url": "/ArifDevJibon/boktita/static/css/main.2f4b79b3.chunk.css"
+  },
+  {
+    "revision": "3d51aa431af69f303303618f45160604",
+    "url": "/ArifDevJibon/boktita/index.html"
+  }
+];
